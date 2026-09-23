b98363846bfda48dcae4dd36de0c8146fdfc4c19
Omkar Arvind Kottawar <144896793+okottawar@users.noreply.github.com>
2026-09-23 15:35:05 +0530

    Allow authorization header through CORS

--- internal/api/middleware.go
+++ internal/api/middleware.go
@@ -38,7 +38,7 @@ func CORSMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, DELETE, OPTIONS")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 		if r.Method == http.MethodOptions {
 			w.WriteHeader(http.StatusNoContent)
 			return
